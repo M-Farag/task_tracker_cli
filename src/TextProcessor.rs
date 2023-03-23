713ebf81b35ab82dc979e4d9c7f29839b157e1dd
M-Farag <mina.farag@icloud.com>
2023-03-24 01:07:10 +0200

    ADD Init
    ADD TextProcessor
    ADD UnitTesting

---
+++ src/TextProcessor.rs
@@ -0,0 +1,14 @@
+use std::num::ParseIntError;
+/// Convert String references to usize
+/// 
+/// # Arguments
+/// 
+/// * `text` - A String reference that holds the text to be parsed
+/// 
+/// # Returns
+/// * `Result<usize, &str
+pub fn convert_text_to_integer(text:&String) -> Result<usize,ParseIntError>
+{
+    let number:usize = text.parse()?;
+    Ok(number)
+}
---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+
+mod TextProcessor;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    //tp is Text Processor
+    #[test]
+    fn test_tp_convert_string_to_usize_works()
+    {
+       let x =  TextProcessor::convert_text_to_integer(&String::from("10")).unwrap();
+       assert_eq!(10,x);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_tp_convert_string_to_usize_can_panic_if_invalid_input()
+    {
+       TextProcessor::convert_text_to_integer(&String::from("abc")).unwrap();
+    }
+
+}
