75e1ec53afe80a5587ff39baa903065c8f8896ee
M-Farag <mina.farag@icloud.com>
2023-03-24 22:06:02 +0200

    UPD Refactor for readability

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,15 @@ use std::env;
 
 fn main() {
     let arguments:Vec<String> = env::args().collect();
-    let mut t1:Task = Task::new(
-         arguments.get(1).expect("Err: Getting task name"),
-         helper::convert_text_to_usize(
-            arguments.get(2).expect("Err: Getting task duration")   
-         ).unwrap()
-         ,
-         arguments.get(3).expect("Err: Getting duration unit")
-    );
+
+    let t_name = arguments.get(1).expect("Err: Getting task name");
+    let t_duration = helper::convert_text_to_usize(
+        arguments.get(2).expect("Err: Getting task duration")   
+     ).unwrap();
+         
+    let t_unit = arguments.get(3).expect("Err: Getting duration unit");
+    
+    let t1:Task = Task::new(t_name,t_duration,t_unit);
 
     println!("Task 1: {:#?}",t1);
 }
