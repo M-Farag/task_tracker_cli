d1f615c219731c1fbfe576dc2b91829eef098a9b
M-Farag <mina.farag@icloud.com>
2023-04-03 21:11:07 +0200

    UPD Main to acheive separation of concerns
    UPD Tast struct to be more simple

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,11 @@ use std::env;
 
 fn main() {
     let arguments:Vec<String> = env::args().collect();
-
-    helper::validate_arguments_length(&arguments, 4).unwrap();
-
-    let t_name = arguments.get(1).expect("Err: Getting task name");
-    let t_duration = helper::convert_text_to_usize(
-        arguments.get(2).expect("Err: Getting task duration")   
-     ).unwrap();
-         
-    let t_unit = arguments.get(3).expect("Err: Getting duration unit");
     
-    let t1:Task = Task::new(t_name,t_duration,t_unit);
+    let t1:Task = Task::new(arguments).unwrap_or_else(|err|{
+        println!("Err: {err}");
+        std::process::exit(1);
+    });
 
     println!("Task 1: {:#?}",t1);
 }
--- src/task.rs
+++ src/task.rs
@@ -1,15 +1,21 @@
 #[derive(Debug)]
-pub struct Task<'a> {
-    name:&'a String,
-    duration:usize,
-    unit:&'a String
+pub struct Task {
+    name:String,
+    duration:String,
+    unit:String
 }
 
-impl<'a> Task<'a> {
+impl Task{
 
-   pub fn new(name:&'a String, duration:usize, unit:&'a String) -> Self
+   pub fn new(arguments:Vec<String>) -> Result<Task,&'static str>
     {
-        Self { name, duration, unit }
+        if arguments.len() < 4 {
+            return Err("Err: Not enough arguments");
+        }
+        let name = arguments.get(1).expect("Err: Getting task name").to_string();
+        let duration = arguments.get(2).expect("Err: Getting task duration").to_string();   
+        let unit = arguments.get(3).expect("Err: Getting duration unit").to_string();
+        Ok(Task { name, duration, unit })
     }
     
 }
