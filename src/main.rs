7e98865bb24ded4523d351625f2b2efad68021b8
M-Farag <mina.farag@icloud.com>
2023-04-12 21:43:07 +0200

    UPD Simplify functionality

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use ttc::task::Task;
-use std::env;
 
 fn main() {
-    let arguments:Vec<String> = env::args().collect();
     
-    let t1:Task = Task::new(arguments).unwrap_or_else(|err|{
+    let t1:Task = Task::new().unwrap_or_else(|err|{
         println!("Err: {err}");
         std::process::exit(1);
     });
--- src/task.rs
+++ src/task.rs
@@ -1,5 +1,5 @@
 use std::{fs, error::Error, io::Write};
-
+use std::env;
 #[derive(Debug)]
 pub struct Task {
     name:String,
@@ -8,46 +8,47 @@ pub struct Task {
 }
 
 impl Task{
-    const TASKS_FILE_NAME:&'static str = "tasks.txt";
-   pub fn new(arguments:Vec<String>) -> Result<Task,Box<dyn Error>>
-    {
-        if arguments.len() < 4 {
-            return Err("Err: Not enough arguments".into());
+        const TASKS_FILE_NAME:&'static str = "tasks.txt";
+        pub fn new() -> Result<Task,Box<dyn Error>>
+        {
+            let arguments:Vec<String> = env::args().collect();
+            if arguments.len() < 4 {
+                return Err("Err: Not enough arguments".into());
+            }
+            let duration = Task::parse_duration(&arguments[2])?;
+            Ok(Task { name: arguments[1].clone(), duration: duration, unit: arguments[3].clone() })
         }
-        let duration = Task::parse_duration(&arguments[2])?;
-        Ok(Task { name: arguments[1].clone(), duration: duration, unit: arguments[3].clone() })
-    }
 
-    pub fn start_task(&self) -> ()
-    {
-        println!("Started task: {},Duration: {}{}", &self.name, &self.duration, &self.unit);
-        let sleep_time = match &self.unit[..] {
-            "s"|"seconds"  => self.duration * 1000,
-            "m"| "minutes" => self.duration * 60 * 1000,
-            "h"| "hours"  => self.duration * 60 * 60 * 1000,
-            _ => 0
-        };
+        pub fn start_task(&self) -> ()
+        {
+            println!("Started task: {},Duration: {}{}", &self.name, &self.duration, &self.unit);
+            let sleep_time = match &self.unit[..] {
+                "s"|"seconds"  => self.duration * 1000,
+                "m"| "minutes" => self.duration * 60 * 1000,
+                "h"| "hours"  => self.duration * 60 * 60 * 1000,
+                _ => 0
+            };
 
-        std::thread::sleep(std::time::Duration::from_millis(sleep_time));
-        println!("Finished task: {}", &self.name);
-    }
+            std::thread::sleep(std::time::Duration::from_millis(sleep_time));
+            println!("Finished task: {}", &self.name);
+        }
 
-    pub fn log_task_to_file(&self)->  Result<bool,Box<dyn Error>>
-    {
-        let mut file_handler= fs::OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open(Task::TASKS_FILE_NAME)?;
+        pub fn log_task_to_file(&self)->  Result<bool,Box<dyn Error>>
+        {
+            let mut file_handler= fs::OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open(Task::TASKS_FILE_NAME)?;
 
-        writeln!(file_handler,"Task: {},Duration: {}{}",&self.name,&self.duration,&self.unit)?;
-        Ok(true)
-    }
+            writeln!(file_handler,"Task: {},Duration: {}{}",&self.name,&self.duration,&self.unit)?;
+            Ok(true)
+        }
 
-    fn parse_duration(duration:&String) -> Result<u64,Box<dyn Error>>
-    {
-        let duration = duration.parse::<u64>()?;
-        Ok(duration)
-    }
+        fn parse_duration(duration:&String) -> Result<u64,Box<dyn Error>>
+        {
+            let duration = duration.parse::<u64>()?;
+            Ok(duration)
+        }
     
 }
 
